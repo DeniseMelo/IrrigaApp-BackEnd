e210fcd08cc479f32042c70a70ac24be100e2518
DeniseMelo <melodenise.ti@gmail.com>
2024-11-01 12:55:33 -0300

    ajuste 6

--- src/main/java/br/com/fiap/IrrigaApp/security/config/MongoConfig.java
+++ src/main/java/br/com/fiap/IrrigaApp/security/config/MongoConfig.java
@@ -5,21 +5,20 @@ import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MongoConfig {
 
     @Bean
+    @ConditionalOnExpression("!T(org.springframework.util.StringUtils).isEmpty('${MONGO_URI:}')")
     public MongoClient mongoClient() {
-        System.out.println(System.getenv("MONGO_URI"));
         String mongoUri = System.getenv("MONGO_URI");
-        if (mongoUri == null || mongoUri.isEmpty()) {
-            throw new IllegalStateException("A variável de ambiente MONGO_URI não está definida");
-        }
         return MongoClients.create(mongoUri);
     }
-    @Bean
-    public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), "bd-irriga-dev");
-    }
 }
+
