2a4016a1f1a33e15d99e79ab54f633327c75ce48
DeniseMelo <melodenise.ti@gmail.com>
2024-11-01 12:15:26 -0300

    ainda ajustes

--- src/main/java/br/com/fiap/IrrigaApp/security/config/MongoConfig.java
+++ src/main/java/br/com/fiap/IrrigaApp/security/config/MongoConfig.java
@@ -4,22 +4,19 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!ci")
 public class MongoConfig {
 
     @Bean
     public MongoClient mongoClient() {
-        System.out.println(System.getenv("MONGO_URI"));
-        String mongoUri = System.getenv("MONGO_URI");
-        if (mongoUri == null || mongoUri.isEmpty()) {
-            throw new IllegalStateException("A variável de ambiente MONGO_URI não está definida");
+        String ciEnv = System.getenv("CI");
+        if (ciEnv != null && ciEnv.equals("true")) {
+            // Se estiver em CI/CD, retorne uma instância "mock" ou null para ignorar o MongoDB
+            return null;
         }
-        return MongoClients.create(mongoUri);
-    }
-    @Bean
-    public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), "bd-irriga-dev");
+        return MongoClients.create(System.getenv("MONGO_URI"));
     }
 }
