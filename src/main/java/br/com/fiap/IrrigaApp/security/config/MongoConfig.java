7ac64f1fba6d29f1493fe3dbd294928e7d966eb7
DeniseMelo <melodenise.ti@gmail.com>
2024-11-02 14:08:31 -0300

    adicionando o nome do bd no properties

--- src/main/java/br/com/fiap/IrrigaApp/security/config/MongoConfig.java
+++ src/main/java/br/com/fiap/IrrigaApp/security/config/MongoConfig.java
@@ -4,12 +4,8 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class MongoConfig {
